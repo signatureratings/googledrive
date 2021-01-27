239eb108b16edf5ebd5ba34a9fe47d3d254c311e
signatureratings <balusairam26@gmail.com>
2021-01-27 20:12:30 +0530

    Changed the name in Navbar

--- src/components/google-drive/Navbar.js
+++ src/components/google-drive/Navbar.js
@@ -1,15 +1,15 @@
-import React from "react"
-import { Navbar, Nav } from "react-bootstrap"
-import { Link } from "react-router-dom"
+import React from 'react'
+import { Navbar, Nav } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 export default function NavbarComponent() {
   return (
-    <Navbar bg="light" expand="sm">
-      <Navbar.Brand as={Link} to="/">
-        WDS Drive
+    <Navbar bg='light' expand='sm'>
+      <Navbar.Brand as={Link} to='/'>
+        SairamBalu Drive
       </Navbar.Brand>
       <Nav>
-        <Nav.Link as={Link} to="/user">
+        <Nav.Link as={Link} to='/user'>
           Profile
         </Nav.Link>
       </Nav>
